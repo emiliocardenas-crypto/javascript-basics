c81f4bd402715f3eec2dc535714854b981a9762a
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-04-22 23:00:21 +0000

    THE big

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-let name = "Emilio";
-console.log(name);
+// let name = "Emilio";
+// console.log(name);
+// let number = 64
+// console.log(number);
+// let age = 11
+// console.log(age);
+// let islearningJS =true 
+let cost = 0
+console.log(cost)
+let funds = 15
+const item1 = "commen beef"
+const item2 = "rare beef"
+const item3 = "epic beef"
+const item4 = "the most exspensive beef in the world and it cant get more exspensive"
+let price1 = 25
+let price2 = 100
+let price3 = 250
+let price4 = 1000000 
+cost=price2+price3
+
+// Order for epic and rare beef; update cost variable
