fce6f28f9f395abb9f0054f1249ddab0e82cf5bc
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-04-27 21:51:14 +0000

    THW

--- app.js
+++ app.js
@@ -7,15 +7,20 @@
 // let islearningJS =true 
 let cost = 0
 console.log(cost)
-let funds = 15
+let funds = 350
 const item1 = "commen beef"
 const item2 = "rare beef"
 const item3 = "epic beef"
-const item4 = "the most exspensive beef in the world and it cant get more exspensive"
+const item4 = "the most exspensive beef in the world and it can't get more exspensive and it is defently not the same as Epic beef"
 let price1 = 25
 let price2 = 100
 let price3 = 250
 let price4 = 1000000 
-cost=price2+price3
+
 
 // Order for epic and rare beef; update cost variable
+cost=price2+price3
+console.log(cost)
+
+// print if i can afford my order
+console.log(funds >= cost)
